01b3f5b33470c4e463d96fb113ba2ca71b4c8bae
net3pang <cn2day@outlook.com>
2025-06-11 15:05:08 +0800

    Site updated: 2025-06-11 15:05:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/10/初见hexo/","2025/06/10/hello-world/"];function toRandomPost(){
+var posts=["2025/06/10/hello-world/","2025/06/10/初见hexo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
